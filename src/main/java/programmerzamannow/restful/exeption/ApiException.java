09d9561f3430cec523b7cb4d45518f22a7c3565e
muhammadffa <muhammaddaffa463@gmail.com>
2024-03-09 00:39:38 +0700

    Create UserController Test

--- src/main/java/programmerzamannow/restful/exeption/ApiException.java
+++
@@ -1,8 +0,0 @@
-package programmerzamannow.restful.exeption;
-
-public class ApiException extends RuntimeException{
-
-    public ApiException(String message) {
-        super(message);
-    }
-}
--- src/main/java/programmerzamannow/restful/service/UserService.java
+++ src/main/java/programmerzamannow/restful/service/UserService.java
@@ -5,9 +5,10 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import programmerzamannow.restful.entity.User;
-import programmerzamannow.restful.exeption.ApiException;
 import programmerzamannow.restful.model.RegisterUserRequest;
 import programmerzamannow.restful.repository.UserRepository;
 import programmerzamannow.restful.security.BCrypt;
@@ -33,7 +34,7 @@ public class UserService {
         }
 
         if(userRepository.existsById(request.getUsername())){
-            throw new ApiException("username already registered");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"username already registered");
         }
 
         User user = new User();
