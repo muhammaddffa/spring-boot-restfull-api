ff80355f19ac98da0aa1f93ba34052ce82e33fd2
muhammaddffa <muhammaddaffa463@gmail.com>
2025-02-27 20:44:41 +0700

    remove address

--- src/main/java/programmerzamannow/restful/controller/AddressController.java
+++ src/main/java/programmerzamannow/restful/controller/AddressController.java
@@ -57,5 +57,16 @@ public class AddressController {
         return WebResponse.<AddressResponse>builder().data(addressResponse).build();
     }
 
+    @DeleteMapping(
+            path = "/api/contacts/{contactId}/addresses/{addressId}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<String> remove(User user,
+                                               @PathVariable("contactId") String contactId,
+                                               @PathVariable("addressId") String addressId){
+        addressService.remove(user, contactId, addressId);
+        return WebResponse.<String>builder().data("OK").build();
+    }
+
 
 }
